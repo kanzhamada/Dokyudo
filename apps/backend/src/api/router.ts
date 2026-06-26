5421c51626066cd6cc6b6c0c6f899a86405c3370
kanzhamada <hamadakanz@gmail.com>
2026-06-26 13:00:30 +0700

    fix: router poc import

--- apps/backend/src/api/router.ts
+++ apps/backend/src/api/router.ts
@@ -1,6 +1,6 @@
 import { createApp } from "../config/hono.ts";
 import { authRoutes } from "../modules/auth/auth.routes.ts";
-import { pocRoutes } from "../modules/poc/poc.routes.ts";
+// import { pocRoutes } from "../modules/poc/poc.routes.ts";
 
 const router = createApp();
 
